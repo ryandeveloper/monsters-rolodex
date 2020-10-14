f56305ee7baf06a3403815124919d22cbfce4190
ryand <ryandumajil@gmail.com>
2020-10-14 22:02:14 +0800

    My First Commit of monsters rolodex

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Card } from '../card/card.component';
+
+import './card-list.style.css';
+
+export const CardList = (props) => (
+   <div className='card-list'>
+      {props.monsters.map(monsters => (
+         <Card key={monsters.id} monster={monsters} />
+      ))}
+   </div>
+)
