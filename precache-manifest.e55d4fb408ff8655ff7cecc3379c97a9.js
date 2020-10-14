e0243071773158d3b0413aae3fc21a53dc21ec31
ryand <ryandumajil@gmail.com>
2020-10-15 03:50:48 +0800

    Updates

---
+++ precache-manifest.e55d4fb408ff8655ff7cecc3379c97a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bf5337b6aff3a85b7c730239d46cdc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1506b61d89a01ee5d629",
+    "url": "/monsters-rolodex/static/css/main.9f68470a.chunk.css"
+  },
+  {
+    "revision": "5ceb71a0b3552c2613c8",
+    "url": "/monsters-rolodex/static/js/2.95da3331.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.95da3331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1506b61d89a01ee5d629",
+    "url": "/monsters-rolodex/static/js/main.c5e6c865.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
